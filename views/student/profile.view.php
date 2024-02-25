56dbd4846f58ba114e8d4a5b1f9b05decdcfaa56
SOKUNTHEA.POEUN <SOKUNTHEA.POEUN@PNC.LAN>
2024-02-25 11:17:12 +0700

    [feat] student profile interface #GEL-156

--- views/student/profile.view.php
+++ views/student/profile.view.php
@@ -4,36 +4,70 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="views/student/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
   <title>Student Profile</title>
 </head>
-<body>    
-  
-  <?php  
-    require 'database/database.php';
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $statement = $connection->prepare("SELECT * FROM users WHERE user_id LIKE :id");
-      $statement->execute([
-          ':id' => $_POST['id']
-      ]);
-      $users = $statement->fetchAll();
-  }
+<body>
+<?php
+require "layouts/header.php";
 
-  foreach ($users as $user):
-  ?>
-  
-  <div class="container text-center mt-5 shadow-lg">
-    <img src="uploading/<?= $user['profile_image']?>" alt="Profile Image" class="rounded-circle" style="width: 200px; height: 200px; object-fit: cover;">
-    <h2 class="mt-3"><?=$user['name'] ?></h2>
-    <p>Email: <?=$user['email'] ?></p>
-    <p>Phone: <?=$user['phone'] ?></p>
-    <p>Gender: <?=$user['gender'] ?></p>
-    <p>Role: Student</p>
-    <p>About Me:</p>
-    <p>I am a good student</p>
+ ?>
+<!-- student_profile -->
+
+<div class="container mt-5 pt-2 shadow-lg  " style="width:700px; height: 500px;">
+
+<!-- start_top -->
+  <div class="top-form ">
+    <div class="gd-profile mb-2 text-center mt-4 ">
+      <div class="p-1 bg-secondary text-dark rounded-circle " style="width: 160px; height: 160px;">
+        <img src="studentprofile/yaya.png" alt="Profile Image" class="rounded-circle mb-2" style="width: 150px; height: 152px;">
+        <h6 class="h6">Poeun Sokunthea</h6>
+        <button type="button" class="btn btn-secondary p-1 mt-1" style="width: 70px;"><i class='fas fa-edit'></i>Edit</button>
+      </div>
+    </div>
+    <div class="form-student">
+      <h3>My Profile</h3>
+      <div class="row  ">
+        <div class="col-6 gap-2">
+        <label for="fname">First name:</label><br>  
+          <div class="p-1 mb-2 border bg-light">Poeun </div>
+        </div>
+        <div class="col-6">
+          <label for="fname">Last name:</label><br>  
+          <div class="p-1 border bg-light">Sokunthea</div>
+        </div>
+      <div class="col-6">
+      <label for="fname">Role:</label><br>  
+        <div class="p-1 border bg-light">Student</div>
+      </div>
+      <div class="col-6">
+      <label for="fname">Phone:</label><br>  
+        <div class="p-1 border bg-light">0974367484</div>
+      </div>
+      <div class="col-6 mt-2">
+      <label for="fname">Email Address:</label><br>  
+        <div class="p-1  border bg-light" style="width: 220%;">sokunthea.poeun@student.passerellesnumeriques.org</div>
+      </div>
+    </div>
   </div>
-  <?php endforeach ?>
+</div>
+<!-- end  -->
+
+<!-- start_bottom -->
+<div class="bottom-form ">
+    <button type="button" class="btn btn-danger " style="width: 90px;">Cancle</button>
+    <button type="button" class="btn btn-success" style="width: 90px;">Save</button>
+</div>
+</div>
+<!-- end -->
+
+
   <!-- Bootstrap JS -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<!-- ends -->
 </body>
 </html>
